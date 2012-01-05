c87d78a03b2152a13a5e0eb73f4033884501aa50
Andrew Havens <email@andrewhavens.com>
2012-01-04 20:43:26 -0800

    initial commit

--- MadChatter/AppDelegate.rb
+++ MadChatter/AppDelegate.rb
@@ -1,15 +1,60 @@
-#
-#  AppDelegate.rb
-#  MadChatter
-#
-#  Created by Andrew Havens on 1/4/12.
-#  Copyright 2012 Devout Design. All rights reserved.
-#
+
+framework "WebKit"
+framework "Growl"
 
 class AppDelegate
     attr_accessor :window
-    def applicationDidFinishLaunching(a_notification)
-        # Insert code here to initialize your application
+    
+    def applicationDidFinishLaunching(notification)
+        load_growl
+        load_web_view
+    end
+    
+    def load_growl
+        GrowlApplicationBridge.setGrowlDelegate(self)
+    end
+    
+    def load_web_view
+        web_view = WebView.new
+        request = NSURLRequest.requestWithURL(NSURL.URLWithString("http://localhost:3000"))
+        web_view.mainFrame.loadRequest(request)
+        
+        web_view.setContinuousSpellCheckingEnabled(true)
+        
+        web_view.frameLoadDelegate = self
+        web_view.setPolicyDelegate(self)
+        window.contentView = web_view
+    end
+    
+    def webView(view, didFinishLoadForFrame:frame)
+        growl_wrapper = MadChatterGrowl.new
+        growl_wrapper.respondsToSelector("send:")        
+        windowScriptObject = view.windowScriptObject
+        windowScriptObject.setValue(growl_wrapper, forKey: "MadChatterGrowl")
+    end
+    
+    # this makes it so links open in the default browser (but it isn't working yet)
+    def webView(view, decidePolicyForNavigationAction:actionInformation, request:request, frame:frame, decisionListener:listener)
+        listener.ignore
+        NSWorkspace.sharedWorkspace.openURL(request.URL)
     end
 end
 
+class MadChatterGrowl
+    def send(title, message)
+        application = NSApplication.sharedApplication
+        unless application.isActive
+            GrowlApplicationBridge.notifyWithTitle(
+                                                   title,
+                                                   description: message,
+                                                   notificationName: "MadChatterGrowlNotification",
+                                                   iconData: nil,
+                                                   priority: 0,
+                                                   isSticky: false,
+                                                   clickContext: nil
+                                                   )
+        end
+    end
+    
+    def self.isSelectorExcludedFromWebScript(sel); false end
+end
