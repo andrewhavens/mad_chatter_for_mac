1cbc24c74ec1ddf5a06502f0b6d323dc803bcd27
Andrew Havens <andrew.havens@evanta.com>
2012-01-12 16:32:58 -0800

    added support for external configuration. also got browser links working

--- MadChatter/AppDelegate.rb
+++ MadChatter/AppDelegate.rb
@@ -3,9 +3,11 @@ framework "WebKit"
 framework "Growl"
 
 class AppDelegate
-    attr_accessor :window
+    attr_accessor :window, :config
     
     def applicationDidFinishLaunching(notification)
+        config_path = NSBundle.mainBundle.pathForResource('AppConfig', ofType: 'plist')
+        @config = load_plist File.read(config_path)
         load_growl
         load_web_view
     end
@@ -16,13 +18,13 @@ class AppDelegate
     
     def load_web_view
         web_view = WebView.new
-        request = NSURLRequest.requestWithURL(NSURL.URLWithString("http://localhost:3000"))
+        request = NSURLRequest.requestWithURL(NSURL.URLWithString(@config['homepage']))
         web_view.mainFrame.loadRequest(request)
         
         web_view.setContinuousSpellCheckingEnabled(true)
         
         web_view.frameLoadDelegate = self
-        web_view.setPolicyDelegate(self)
+        web_view.policyDelegate = self
         window.contentView = web_view
     end
     
@@ -33,10 +35,10 @@ class AppDelegate
         windowScriptObject.setValue(growl_wrapper, forKey: "MadChatterGrowl")
     end
     
-    # this makes it so links open in the default browser (but it isn't working yet)
-    def webView(view, decidePolicyForNavigationAction:actionInformation, request:request, frame:frame, decisionListener:listener)
-        listener.ignore
+    # this makes it so links open in the default browser
+    def webView(view, decidePolicyForNewWindowAction:actionInformation, request:request, newFrameName:frameName, decisionListener:listener)
         NSWorkspace.sharedWorkspace.openURL(request.URL)
+        listener.ignore
     end
 end
 
